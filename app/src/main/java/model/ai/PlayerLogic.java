bfd1f7ecf82a577609d2f90cbdfe2b114e21b85c
Arpad Fodor <37120889+arpadfodor@users.noreply.github.com>
2019-04-14 15:34:18 +0200

    Simplify AI matrix adapter - step depends only on the neural net output sureness

--- app/src/main/java/model/ai/PlayerLogic.java
+++ app/src/main/java/model/ai/PlayerLogic.java
@@ -53,101 +53,36 @@ public class PlayerLogic {
      */
     public Integer[] CalculateStep(int[][][] playground_state, int player_Id_to_step_for) {
 
-        //adapter
-
-        int[] most_promising_field = new int[3];
-        most_promising_field[0] = 1;
-        most_promising_field[1] = 1;
-        most_promising_field[2] = 100;
-
-        int[] least_promising_field = new int[3];
-        least_promising_field[0] = 1;
-        least_promising_field[1] = 1;
-        least_promising_field[2] = 0;
-
-        int fieldsOfPlayer = 0;
+        Integer[] coordinates = new Integer[2];
+        coordinates[0] = 0;
+        coordinates[1] = 0;
+        Double max_prediction = 0.0;
 
         //playground pre-processing
         for(int actual_height = 1; actual_height < playground_state.length-1; actual_height++){
 
-            for(int actual_width = 1; actual_width < playground_state[actual_width].length-1; actual_width++){
+            for(int actual_width = 1; actual_width < playground_state[actual_height].length-1; actual_width++){
 
-                int current_most_promising_value = 0;
-                int current_least_promising_value = 100;
-                int fields_clickable = 0;
+                int owner = playground_state[actual_height][actual_width][0];
 
-                for(int i = actual_height-1; i <= actual_height+1; i++){
+                if(owner == player_Id_to_step_for || owner == 0){
 
-                    for(int j = actual_width-1; j <= actual_width+1; j++){
+                    int[] current_field = {actual_height, actual_width};
+                    Double[][] selected_matrix = RetrieveSelectedSubMatrix(current_field, playground_state, player_Id_to_step_for);
+                    //feeding the Neural Network with the selected 3x3 matrix
+                    Double[] result = NeuralNetworkStep(selected_matrix);
 
-                        current_most_promising_value += playground_state[i][j][2];
-                        current_least_promising_value += playground_state[i][j][2];
-
-                        int current_owner_Id = playground_state[i][j][0];
-
-                        if(current_owner_Id == 0 || current_owner_Id == player_Id_to_step_for){
-                            fields_clickable++;
-                        }
-
-                        if(current_owner_Id == player_Id_to_step_for){
-                            fieldsOfPlayer++;
-                        }
-
+                    if(result[2] > max_prediction){
+                        //translates the Neural Network selection coordinates to global coordinates
+                        coordinates[0] = (result[0].intValue()) + actual_height - 1;
+                        coordinates[1] = (result[1].intValue()) + actual_width - 1;
+                        max_prediction = result[2];
                     }
 
                 }
 
-                if(current_most_promising_value < most_promising_field[2] && fields_clickable > 0){
-                    most_promising_field[0] = actual_height;
-                    most_promising_field[1] = actual_width;
-                    most_promising_field[2] = current_most_promising_value;
-                }
-
-                if(current_least_promising_value > least_promising_field[2] && fields_clickable > 0){
-                    least_promising_field[0] = actual_height;
-                    least_promising_field[1] = actual_width;
-                    least_promising_field[2] = current_least_promising_value;
-                }
-
             }
 
-        }
-
-        Double[][] selected_matrix;
-        Integer[] coordinates;
-
-        //if limit reached, prioritise corner stepping
-        if(most_promising_field[2] > SUBMATRIX_LIMIT_ABOVE_CORNERS_PRIORISED){
-            coordinates = EmptyCornerStep(playground_state, player_Id_to_step_for);
-
-            if(coordinates != null){
-                return coordinates;
-            }
-
-        }
-
-        //if limit reached, step to the least promising sub-matrix
-        if(fieldsOfPlayer < NUMBER_OF_FIELDS_LIMIT_ESCAPING_BELOW){
-            selected_matrix = RetrieveSelectedSubMatrix(least_promising_field, playground_state, player_Id_to_step_for);
-
-            //feeding the Neural Network with the selected 3x3 matrix
-            coordinates = NeuralNetworkStep(selected_matrix);
-
-            //translates the Neural Network selection coordinates to global coordinates
-            coordinates[0] += least_promising_field[0]-1;
-            coordinates[1] += least_promising_field[1]-1;
-        }
-
-        //step to the most promising sub-matrix
-        else{
-            selected_matrix = RetrieveSelectedSubMatrix(most_promising_field, playground_state, player_Id_to_step_for);
-
-            //feeding the Neural Network with the selected 3x3 matrix
-            coordinates = NeuralNetworkStep(selected_matrix);
-
-            //translates the Neural Network selection coordinates to global coordinates
-            coordinates[0] += most_promising_field[0]-1;
-            coordinates[1] += most_promising_field[1]-1;
         }
 
         return coordinates;
@@ -158,12 +93,11 @@ public class PlayerLogic {
      * Feeds the Neural Network with the given 3x3 matrix
      *
      * @param	input_matrix    [i] is Y coordinate, [][j] is X coordinate, [i][j] is the double value: minus value means AI Player cannot step onto that Field; the bigger value is the better
-     * @return 	Integer[]       Local coordinates of the Field to step on the input matrix: [0] is Y, [1] is X coordinate
+     * @return 	Integer[]       Local coordinates of the Field to step on the input matrix: [0] is Y, [1] is X coordinate, [2] is the prediction value - 1 is the greatest
      */
-    private Integer[] NeuralNetworkStep(Double[][] input_matrix){
+    private Double[] NeuralNetworkStep(Double[][] input_matrix){
 
-        Integer[] coordinates = new Integer[2];
-
+        Double[] result = new Double[3];
         INDArray actualInput = Nd4j.zeros(1,input_matrix.length * input_matrix[0].length);
 
         //building-up DeepLearning4J-specific input
@@ -191,10 +125,11 @@ public class PlayerLogic {
         }
 
         //translating output neuron index to local Field coordinates
-        coordinates[0] = maximum_prediction_index / 3;
-        coordinates[1] = maximum_prediction_index % 3;
+        result[0] = (maximum_prediction_index / 3) + 0.0;
+        result[1] = (maximum_prediction_index % 3) + 0.0;
+        result[2] = maximum_prediction;
 
-        return coordinates;
+        return result;
 
     }
 
@@ -250,7 +185,6 @@ public class PlayerLogic {
     private Double[][] RetrieveSelectedSubMatrix(int[] most_promising_field, int[][][] playground_state, int player_Id_to_step_for){
 
         Double[][] selected_matrix = new Double[3][3];
-
         //normalization multiplier for neural network
         double foreToken = 0.25;
 
